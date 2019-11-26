adb528b45106b70a143277b2135613e8cd9322d3
Tara Z. Manicsic <tzmanics@gmail.com>
2019-11-25 22:09:50 -0500

    changes to ng add adds config file and test

--- src/anglify-schematics/index.ts
+++ src/anglify-schematics/index.ts
@@ -1,10 +1,21 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
-
+import { Rule, SchematicContext, Tree } from "@angular-devkit/schematics";
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function anglifySchematics(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
+    // 1) create config file
+    const config = {
+      apiId: "",
+      accessToken: "",
+      projectName: ""
+    };
+    tree.create("/netlifyConfig.json", JSON.stringify(config, null, 2));
+
+    // 2) update gitignore file #thanksBill
+
+    // 3) call Netlify API
+
     return tree;
   };
 }
