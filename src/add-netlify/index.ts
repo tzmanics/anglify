0ad14f36ef9a91c856d34f489bab8455c2df27c1
Tara Z. Manicsic <tzmanics@gmail.com>
2019-12-04 22:43:58 -0500

    creates files to start add-netlify task

---
+++ src/add-netlify/index.ts
@@ -0,0 +1,5 @@
+import { Rule, SchematicContext, Tree } from "@angular-devkit/schematics/tasks";
+
+export function addNetlify(options: any): Rule {
+  return (host: Tree, context: SchematicContext) => {};
+}
